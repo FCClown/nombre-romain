a39d18d1a8b5d417a0cadd778e991d8418aa4f0d
FCClown <titou246@gmx.fr>
2022-10-28 23:19:04 +0200

    Add bash file

---
+++ nombres romain/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git  fetch
+
+git checkout dev
+
+npm build
+
+buildResult=$?
+
+if [$buildResult -ne 0]; then
+    echo "Build error"
+    git tag failures/test
+    git reset --hard <>
+    git push -f origin dev
+    exit $buildResult
+fi
+
+npm test
+
+testResult=$?
+
+if [$testResult -ne 0]; then
+    echo "test error"
+    git tag failures/test
+    git reset --hard <>
+    git push -f origin dev
+    exit $testResult
+fi
+
+npm clean
+
+git checkout main
+git merge dev
+git push main
+
+exit 0
