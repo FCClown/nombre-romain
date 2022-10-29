6194b3c0468160f3c52e5958c45d373568431dfa
FCClown <titou246@gmx.fr>
2022-10-29 15:10:49 +0200

    Test OK chiffre de 1 à 3

--- nombres romain/sum.js
+++ nombres romain/sum.js
@@ -1,68 +1,70 @@
-
-
-function convertisseurNombreRomain(a) {
-
-    switch(a) {
-        case 1:
-            return "I"
-        case 2:
-            return "II"
-        case 3:
-            return "III"
-        case 4:
-            return "IV"
-        case 5:
-            return "V"
-        case 6:
-            return "VI"
-        case 7:
-            return "VII"
-        case 8:
-            return "VIII"
-        case 9:
-            return "IX"
-        case 10:
-            return "X"
-        case 11:
-            return "XI"
-        case 12:
-            return "XII"
-        case 13:
-            return "XIII"
-        case 14:
-            return "XIV"
-        case 15:
-            return "XV"
-        case 16:
-            return "XVI"
-        case 17:
-            return "XVII"
-        case 18:
-            return "XVIII"
-        case 19:
-            return "XIX"
-        case 20:
-            return "XX"
-        case 21:
-            return "XXI"
-        case 22:
-            return "XXII"
-        case 23:
-            return "XXIII"
-        case 24:
-            return "XXIV"
-        case 25:
-            return "XXV"
-        case 26:
-            return "XXVI"
-        case 27:
-            return "XXVII"
-        case 28:
-            return "XXVIII"
-        case 29:
-            return "XXIX"
-        case 30:
-            return "XXX"
+const SYMBOL_CONFIGS = [
+    {
+      character: 'M',
+      value: 1000,
+      stepDownAmount: 100,
+    },
+    {
+      character: 'D',
+      value: 500,
+      stepDownAmount: 100,
+    },
+    {
+      character: 'C',
+      value: 100,
+      stepDownAmount: 10,
+    },
+    {
+      character: 'L',
+      value: 50,
+      stepDownAmount: 10,
+    },
+    {
+      character: 'X',
+      value: 10,
+      stepDownAmount: 1,
+    },
+    {
+      character: 'V',
+      value: 5,
+      stepDownAmount: 1,
+    },
+    {
+      character: 'I',
+      value: 1,
+      stepDownAmount: 0,
+    },
+  ];
+  
+  function handleThresholdCase({number, config}) {
+    const {value, character, stepDownAmount} = config;
+  
+    let numeral = '';
+    let remainder = number - value;
+    if (number < value) {
+      const stepDownCharacter = SYMBOL_CONFIGS.find(
+        c => c.value === stepDownAmount,
+      ).character;
+      numeral += stepDownCharacter;
+      remainder += stepDownAmount;
     }
-}
-module.exports = convertisseurNombreRomain;
+    numeral += character;
+    numeral += numberToRomanNumeral(remainder);
+    return numeral;
+  }
+  
+  function numberToRomanNumeral(number) {
+    for (let i = 0; i < SYMBOL_CONFIGS.length; i += 1) {
+      const config = SYMBOL_CONFIGS[i];
+      const {value, stepDownAmount} = config;
+      if (number >= value - stepDownAmount) {
+        return handleThresholdCase({
+          number,
+          config,
+        });
+      }
+    }
+
+    return '';
+  }
+  module.exports = numberToRomanNumeral;
--- nombres romain/sum.test.js
+++ nombres romain/sum.test.js
@@ -1,45 +1,13 @@
-const convertisseurNombreRomain = require('./sum');
+const numberToRomanNumeral = require('./sum');
 
-test('convert 1 to I', () => {
-    expect(convertisseurNombreRomain(1)).toEqual("I");
-});
-
-test('convert 2 to II', () => {
-    expect(convertisseurNombreRomain(2)).toEqual("II");
-});
-
-test('convert 3 to III', () => {
-    expect(convertisseurNombreRomain(3)).toEqual("III");
-});
-
-test('convert 4 to IV', () => {
-    expect(convertisseurNombreRomain(4)).toEqual("IV");
-});
-
-test('convert 5 to V', () => {
-    expect(convertisseurNombreRomain(5)).toEqual("V");
-});
-
-test('convert 6 to VI', () => {
-    expect(convertisseurNombreRomain(6)).toEqual("VI");
-});
-
-test('convert 7 to VII', () => {
-    expect(convertisseurNombreRomain(7)).toEqual("VII");
-});
-
-test('convert 8 to VIII', () => {
-    expect(convertisseurNombreRomain(8)).toEqual("VIII");
-});
-
-test('convert 9 to IX', () => {
-    expect(convertisseurNombreRomain(9)).toEqual("IX");
-});
-
-test('convert 10 to X', () => {
-    expect(convertisseurNombreRomain(10)).toEqual("X");
-});
-
-test('convert 11 to XI', () => {
-    expect(convertisseurNombreRomain(11)).toEqual("X1");
+describe('romanNumerals', () => {
+  describe('numberToRomanNumeral', () => {
+    it.each([
+      [1, 'I'],
+      [2, 'II'],
+      [3, 'III'],
+    ])('converts %i to %s', (decimalNumber, romanNumeral) => {
+      expect(numberToRomanNumeral(decimalNumber)).toEqual(romanNumeral);
+    });
+  });
 });
