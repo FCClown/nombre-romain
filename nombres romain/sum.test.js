0ec750d8c244aa8904e36b589d4fc142979cbbee
FCClown <titou246@gmx.fr>
2022-10-29 15:38:13 +0200

    Gestion des tests de 1 à 20

--- nombres romain/sum.test.js
+++ nombres romain/sum.test.js
@@ -15,6 +15,16 @@ describe('romanNumerals', () => {
       [8, 'VIII'],
       [9, 'IX'],
       [10, 'X'],
+      [11, 'XI'],
+      [12, 'XII'],
+      [13, 'XIII'],
+      [14, 'XIV'],
+      [15, 'XV'],
+      [16, 'XVI'],
+      [17, 'XVII'],
+      [18, 'XVIII'],
+      [19, 'XIX'],
+      [20, 'XX'],
     ])('converts %i to %s', (decimalNumber, romanNumeral) => {  // QUAND : On lui donne <decimalNumber>
       expect(numberToRomanNumeral(decimalNumber)).toEqual(romanNumeral); // ALORS : On obtient <romanNumeral>
     });
